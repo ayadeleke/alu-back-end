2c93658595745950ed3b1ad6fd12e11651e7413f
ayadeleke <https://github.com/ayadeleke>
2023-07-06 04:25:57 -0700

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 """
-    python script that returns TODO list progress for a given employee ID
+    Python script that returns TODO list progress for a given employee ID
 """
 import json
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    """ Functions for gathering  data from an API """
-    request_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
+    # Functions for gathering data from an API
+    employee_id = argv[1]
+    request_employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
     employee = json.loads(request_employee.text)
-    employee_name = employee.get("OK")
-    request_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
-    tasks = {}
+    employee_name = employee.get("name")
+
+    request_todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id))
     employee_todos = json.loads(request_todos.text)
 
+    tasks = {}
     for dictionary in employee_todos:
         tasks.update({dictionary.get("title"): dictionary.get("completed")})
 
-    EMPLOYEE_NAME = employee_name
     TOTAL_NUMBER_OF_TASKS = len(tasks)
     NUMBER_OF_DONE_TASKS = len([k for k, v in tasks.items() if v is True])
-    print("Employee {} is done with tasks({}/{}):"
-            .format(
-                EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for k, v in tasks.items():
         if v is True:
-            print("\t {}".format(k))
+            print("\t{}".format(k))
