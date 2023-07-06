82221db96389344e3fb647c28699d478282c6959
ayadeleke <https://github.com/ayadeleke>
2023-07-06 05:03:54 -0700

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""A Python script to export data in the CSV format"""
+"""a Python script to export data in the CSV format"""
 
 import requests
 import sys
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    todo = todo.format(employee_id)
 
-    user_info = requests.get(url).json()
-    todo_info = requests.get(todo_url).json()
+    user_info = requests.request("GET", url).json()
+    todo_info = requests.request("GET", todo).json()
 
     employee_name = user_info.get("name")
     employee_username = user_info.get("username")
-    total_tasks = [task for task in todo_info if task["completed"]]
+    total_tasks = list(filter(lambda x: (x["completed"] is True), todo_info))
     task_com = len(total_tasks)
     total_task_done = len(todo_info)
 
-    with open(f"{employee_id}.csv", "w") as f:
-        for task in todo_info:
-            f.write(
-                f'"{employee_id}",'
-                f'"{employee_username}",'
-                f'"{str(task["completed"])}",'
-                f'"{task["title"]}",'
-                "\n"
-            )
-
+    with open(str(employee_id) + '.csv', "w") as f:
+        [f.write('"' + str(employee_id) + '",' +
+                '"' + employee_username + '",' +
+                '"' + str(task["completed"]) + '",' +
+                '"' + task["title"] + '",' + "\n")
+        for task in todo_info]
