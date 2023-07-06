3415e0806edffa27c42a5aa837cda73f13d09fdb
ayadeleke <https://github.com/ayadeleke>
2023-07-06 04:54:12 -0700

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,13 +9,15 @@ from sys import argv
 if __name__ == "__main__":
     # Functions for gathering data from an API
     employee_id = argv[1]
-    request_employee = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-            .format(employee_id))
+    request_employee = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
+    )
     employee = json.loads(request_employee.text)
     employee_name = employee.get("name")
 
-    request_todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-            .format(employee_id))
+    request_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)
+    )
     employee_todos = json.loads(request_todos.text)
 
     tasks = {}
@@ -26,7 +28,8 @@ if __name__ == "__main__":
     NUMBER_OF_DONE_TASKS = len([k for k, v in tasks.items() if v is True])
 
     print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+        employee_name, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
+    ))
     for k, v in tasks.items():
         if v is True:
             print("\t{}".format(k))
